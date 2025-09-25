469542ab26fece6494d031dde9cd559f1ddf7206
mounika <mounikapunuguti@gmail.com>
2025-09-25 22:35:46 +0530

    some message

--- 04-read.sh
+++ 04-read.sh
@@ -2,6 +2,6 @@
 
 echo "please enter your pin number"
 
-read pin number
+read -s pin number #here pin number is variable and read -s inicates user will get access to enter his pin number and number will not appear on screen
 
 echo "Entere pin number is $pin number"
