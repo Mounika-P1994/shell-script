1e7bcea58d19ba030e272242852282dc002b7c1b
mounika <mounikapunuguti@gmail.com>
2025-09-25 22:13:48 +0530

    message

--- 03-variables1.sh
+++ 03-variables1.sh
@@ -10,3 +10,11 @@ echo "$PERSON1:: Welcome to Alaska $PERSON2"
 echo "$PERSON2:: Thank you $PERSON1, Finally not on Zoom Webinar"
 echo "$PERSON1:: So, do we have a deal"
 echo "$PERSON2:: Yes, if you make Ukraine joins Russia"
+
+
+x=$1
+y=$2
+
+sum=$((x+y))
+
+echo "sum of two numbers is :: $sum
