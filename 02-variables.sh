bcd24b5273cb055ea637a26ac6972bf76b153bab
mounika <mounikapunuguti@gmail.com>
2025-09-25 12:16:38 +0530

    shell practice

---
+++ 02-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+Person1="rajesh"
+Person2="suresh"
+
+echo "$Person1:: Hi Person2, How are you?"
+echo "$Person2:: I'm fine. Whata about you?"
+echo "$Person1:: Fine, What you are learning currently"
+echo "$Person2:: I'm learning shell script"
+
