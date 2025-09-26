3ee52eaad2059524678f1d63da0f2d5776b719b3
mounika <mounikapunuguti@gmail.com>
2025-09-26 23:16:58 +0530

    script

---
+++ 07-environment.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export course = "Devsecops with AWS" 
+
+# this variableis valid until the process will be alive , it will be accessable to subprocess as well
+# euser s 
+
+#echo "course name is" $course
+
