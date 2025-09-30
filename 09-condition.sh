968632df92d36fdaff63dc5f9767fb8d356a5d71
mounika <mounikapunuguti@gmail.com>
2025-09-30 13:17:14 +0530

    some message

--- 09-condition.sh
+++ 09-condition.sh
@@ -7,10 +7,11 @@ echo "enter a number"
 if [$number -le 200]; then
 
 echo " Given number $number is less than 200"
+
 else if [$number -ge 200]; then
 
 echo " Given number $number is greater than 200"
 
 else 
- echo " Given number $number is equal to 200"
+ echo " Given number $number 
 
