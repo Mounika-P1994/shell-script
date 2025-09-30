bef21a0e2d280d3063a160728934e79b3e32b882
mounika <mounikapunuguti@gmail.com>
2025-09-30 12:55:59 +0530

    some message

---
+++ 09-if condition.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+number=100,
+
+read "enter a number"
+
+if [$number -le 200]; then
+
+echo " Given number $number is less than 200"
+else if [$number -ge 200]; then
+
+echo " Given number $number is greater than 200"
+
+else 
+ echo " Given number $number is equal to 200"
+
