7aaa69ab3794b215673f5159d3d994353515d14e
mounika <mounikapunuguti@gmail.com>
2025-09-30 13:32:23 +0530

    somemessage

--- 09-condition.sh
+++ 09-condition.sh
@@ -8,7 +8,7 @@ if [ $number -lt 200]; then
 
 echo "Given number $number is less than 200"
 
-else if [ $number -gt 200]; then
+elif [ $number -gt 200]; then
 
 echo "Given number $number is greater than 200"
 fi
